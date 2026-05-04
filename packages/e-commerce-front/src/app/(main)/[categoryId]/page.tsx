6508910ae3d25be8d5e3e9a4be4778ff2557f563
hoanganh <ndha1115@gmail.com>
2026-05-04 22:20:17 +0700

    fix: eslint
    #100

--- packages/e-commerce-front/src/app/(main)/[categoryId]/page.tsx
+++ packages/e-commerce-front/src/app/(main)/[categoryId]/page.tsx
@@ -2,6 +2,7 @@ import Category from "@/features/main/category/Category";
 
 const CategoryPage = async ({ params }: PageProps<"/[categoryId]">) => {
   const { categoryId } = await params;
+  console.log("Category ID:", categoryId); // Debug log to check the categoryId value
   return <Category />;
 };
 
