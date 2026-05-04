2f942aa36767d22b28a7461c6a0980198b69764a
hoanganh <ndha1115@gmail.com>
2026-05-04 21:45:00 +0700

    Revert "test: test ci"
    This reverts commit e7b711ebfc6db704657304f06969e87e4ed03d1b.

--- packages/e-commerce-api/src/api/v1/auth/auth/auth.controller.ts
+++ packages/e-commerce-api/src/api/v1/auth/auth/auth.controller.ts
@@ -42,7 +42,6 @@ export class AuthController implements BaseAuthControllerInterface {
    * POST /auth/register
    */
   async postRegister(body: PostRegisterBody): Promise<PostRegister200Response> {
-    console.log('test ci');
     return await this.service.postRegister(body);
   }
 }
