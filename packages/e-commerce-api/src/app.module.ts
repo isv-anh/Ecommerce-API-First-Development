4451df04c97d876f93222b41bc51bca5e9ba9a6f
hoanganh <ndha1115@gmail.com>
2026-05-04 19:41:23 +0700

    feat: shop api
    #97

--- packages/e-commerce-api/src/app.module.ts
+++ packages/e-commerce-api/src/app.module.ts
@@ -5,6 +5,7 @@ import { HealthModule } from './health.module';
 import { AuthModule } from './api/v1/auth/auth/auth.module';
 import { CategoriesModule } from '@/api/v1/product/categories/categories.module';
 import { BrandsModule } from '@/api/v1/product/brands/brands.module';
+import { ShopsModule } from '@/api/v1/shop/shops/shops.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { BrandsModule } from '@/api/v1/product/brands/brands.module';
     AuthModule,
     CategoriesModule,
     BrandsModule,
+    ShopsModule,
   ],
 })
 export class AppModule {}
