1105d86a8a9e2705b5381925fb14f61b7163f029
hoanganh <ndha1115@gmail.com>
2026-06-07 21:19:33 +0700

    Revert "chore: remove file unused"
    This reverts commit 81eaf6e401f9448ff7408ccc50c5ee4a4cfbab20.

---
+++ scripts/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ENV_FILE=".env"
+
+ENV_E_COMMERCE_API="./packages/e-commerce-api/.env"
+
+echo "setup environtment"
+
+read -p "enter DB_USERNAME: " username
+read -s -p "enter DB_PASSWORD: " password
+read -p "enter DB_HOST (default: localhost): " db_host
+read -p "enter DB_PORT (default: 5432): " db_port
+read -p "enter DB_NAME: " db_name
+read -p "enter SEFT_HOSTED_RUNNER_TOKEN: " seft_token
+read -p "enter JWT_SECRET: " jwt_secret
+
+if [ -z "$db_host" ]; then
+    db_host="localhost"
+fi
+if [ -z "$db_port" ]; then
+    db_port="5432"
+fi
+
+echo "DB_USERNAME=$username" > "$ENV_FILE"
+echo "DB_PASSWORD=$password" >> "$ENV_FILE"
+echo "DB_USERNAME=$username" > "$ENV_E_COMMERCE_API"
+echo "DB_PASSWORD=$password" >> "$ENV_E_COMMERCE_API"
+
+echo "DB_HOST=$db_host" >> "$ENV_FILE"
+echo "DB_PORT=$db_port" >> "$ENV_FILE"
+echo "DB_NAME=$db_name" >> "$ENV_FILE"
+echo "DB_HOST=$db_host" >> "$ENV_E_COMMERCE_API"
+echo "DB_PORT=$db_port" >> "$ENV_E_COMMERCE_API"
+echo "DB_NAME=$db_name" >> "$ENV_E_COMMERCE_API"
+
+echo "RUNNER_TOKEN=$seft_token" >> "$ENV_FILE"
+echo "JWT_SECRET=$jwt_secret" >> "$ENV_FILE"
+echo "JWT_SECRET=$jwt_secret" >> "$ENV_E_COMMERCE_API"
+
+echo "UID=$(id -u)" >> "$ENV_FILE"
+echo "GID=$(id -g)" >> "$ENV_FILE"
+
+echo "saved $ENV_FILE"
