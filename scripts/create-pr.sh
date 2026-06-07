a3956a156d82fbe7146d086618f447fbf22a68e0
hoanganh <ndha1115@gmail.com>
2026-06-07 20:40:39 +0700

    Create PR

--- scripts/create-pr.sh
+++ scripts/create-pr.sh
@@ -14,41 +14,41 @@ fi
 echo "user: $USERNAME"
 
 # get issues assigned to user
-ISSUES=$(gh issue list -R "$OWNER/$REPO" --assignee "$USERNAME" --json number,title -q '.[] | "\(.number): \(.title)"')
+ISSUES=$(gh issue list \
+    -R "$OWNER/$REPO" \
+    --assignee "$USERNAME" \
+    --json number,title \
+    -q '.[] | "\(.number): \(.title)"')
 
 if [ -z "$ISSUES" ]; then
-    echo "❗ No issues assigned to you could be found $OWNER/$REPO."
+    echo "❗ No issues assigned to you could be found in $OWNER/$REPO."
     exit 0
 fi
 
-echo -e "\n📋 Your issues:"
-IFS=$'\n' read -rd '' -a ISSUE_LIST <<<"$ISSUES"
+# select issue with fzf
+SELECTED=$(echo "$ISSUES" | fzf \
+    --prompt="📋 Select issue: " \
+    --height=40% \
+    --border)
 
-for i in "${!ISSUE_LIST[@]}"; do
-    echo "$((i + 1)). ${ISSUE_LIST[$i]}"
-done
-
-# Choice issuse
-echo -ne "\n🔢 choice issuse (or press Enter to exit): "
-read CHOICE
-
-if [[ -z "$CHOICE" || "$CHOICE" -lt 1 || "$CHOICE" -gt "${#ISSUE_LIST[@]}" ]]; then
+if [ -z "$SELECTED" ]; then
     echo "👋 Exit."
     exit 0
 fi
 
-SELECTED="${ISSUE_LIST[$((CHOICE - 1))]}"
+ISSUE_NUMBER=$(echo "$SELECTED" | cut -d':' -f1)
+ISSUE_TITLE=$(echo "$SELECTED" | cut -d':' -f2- | xargs)
 
 BRANCH_NAME=$(echo "$SELECTED" \
   | tr '[:upper:]' '[:lower:]' \
   | sed -E 's/[^a-z0-9]+/-/g' \
   | sed -E 's/^-+|-+$//g')
 
-ISSUE_NUMBER=$(echo "$SELECTED" | cut -d':' -f1)
-
-ISSUE_TITLE=$(echo "$SELECTED" | cut -d':' -f2- | xargs)
-
-LABELS=$(gh issue view "$ISSUE_NUMBER" --repo "$OWNER/$REPO" --json labels --jq '.labels[].name' | paste -sd "," -)
+LABELS=$(gh issue view "$ISSUE_NUMBER" \
+    --repo "$OWNER/$REPO" \
+    --json labels \
+    --jq '.labels[].name' \
+    | paste -sd "," -)
 
 git checkout -b "$BRANCH_NAME"
 
@@ -57,6 +57,7 @@ git commit --allow-empty -m "Create PR"
 git push -u origin "$BRANCH_NAME"
 
 gh pr create \
+  --draft \
   --repo "$OWNER/$REPO" \
   --title "$ISSUE_TITLE" \
   --body "Closes #$ISSUE_NUMBER" \
